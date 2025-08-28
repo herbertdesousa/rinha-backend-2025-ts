5063460ae87a866bf2ac0c337789d528691ab160
herbertdesousa <sousaherbert138@gmail.com>
2025-08-28 20:09:46 -0300

    feat: basic integration with payments-processor

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,43 @@
+import { config } from "dotenv";
+config();
+
 import Fastify from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import axios from "axios";
+
+const env = {
+  PROCESSOR_DEFAULT_URL: process.env["PROCESSOR_DEFAULT_URL"]!,
+  PROCESSOR_FALLBACK_URL: process.env["PROCESSOR_FALLBACK_URL"]!,
+};
+
+const processorApi = axios.create({
+  baseURL: env.PROCESSOR_DEFAULT_URL,
+  headers: {
+    'X-Rinha-Token': '123',
+  },
+});
+const processorFallbackApi = axios.create({
+  baseURL: env.PROCESSOR_FALLBACK_URL,
+  headers: {
+    'X-Rinha-Token': '123',
+  },
+});
 
 const fastify = Fastify({ logger: true });
 fastify.register(fastifyCors, { origin: "*" });
 
 fastify.get("/payments-summary", async () => {
-  return { hello: "world" };
+  console.log(env);
+
+  const [defaultResponse, fallbackResponse] = await Promise.all([
+    processorApi.get("/admin/payments-summary"),
+    processorFallbackApi.get("/admin/payments-summary"),
+  ]);
+
+  return {
+    default: defaultResponse.data,
+    fallback: fallbackResponse.data,
+  };
 });
 
 fastify.post("/payments", async () => {
